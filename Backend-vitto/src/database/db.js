4f6c2ba9ce3eeed9abfc0ccd89ec57eb2fc91fb5
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2026-05-07 14:47:43 +0530

    db config changes

--- Backend-vitto/src/database/db.js
+++ Backend-vitto/src/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/vitto_loan_db');
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MongoDB Connected");
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
